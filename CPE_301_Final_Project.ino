b234e8fba37eca284209efbb4f12cfd33ceedc72
Owen Cespon <59898060+1102-Cespon-Owen@users.noreply.github.com>
2024-05-10 12:39:43 -0700

    re write some code

--- CPE_301_Final_Project.ino
+++ CPE_301_Final_Project.ino
@@ -7,15 +7,12 @@ void setup() {
 // Need to finish states/double check the states. On Friday. start looking at non library stuff
 void loop() {
   // put your main code here, to run repeatedly:
-  bool error, idle, start, reset, fan, disabled, moniWater, moniTemp; 
+  bool error, idle, start, reset, fan, disabled; 
   bool gLED = 0, yLED = 0, rLED = 0,  bLED = 0;  
-  double waterLvl = 0, temp = 0, humidity = 0, maxTemp = 0, thresholdWtr = 0; 
+  double waterLvl = 0, temp = 0, humidity = 0, maxTemp = 0. thresholdWtr = 0; 
 
 
-  if(start == 1){
-    //THIS IS WHERE WE WILL CALL CHECKWATER AND CHECKTEMP FUNCTIONS
-    moniWater = 1;
-    moniTemp = 1;
+  if(start = 1){
 
     // CHECKING IF TEMP IS GREATER THAN MAX TO TURN ON COOLER
     if(temp > maxTemp){
@@ -30,7 +27,7 @@ void loop() {
       // need to display temp and humidity
 
     // TURNS THE COOLER TO IDLE IF TEMP IS BELOW THE MAXTEMP
-    }if(temp <= maxTemp){
+    } else if(temp <= maxTemp){
       idle = 1; // means idling
       fan = 0; 
 
@@ -43,7 +40,7 @@ void loop() {
     } 
     
     // ERROR CHECKING FOR WATER LEVEL
-    if(waterLvl < thresholdWtr){
+    if(waterLvl <= thresholdWtr){
       println("Water level is too low");
       error = 1; 
       idle = 0; 
@@ -55,39 +52,35 @@ void loop() {
 
 
       // Check if reset button is pressed
-      if(reset == 1 && waterLvl >= thresholdWtr){
+      if(reset == 1){
         idle = 1;
 
         rLED = 0;
         bLED = 0;
         yLED = 0;
         gLED = 1; 
-      }    
+      }
+
+
+    
     }
+      // if start = 0 anywhere
+      if(start == 0){
+        fan = 0; 
+        disabled = 1;
+
+        rLED = 0;
+        bLED = 0;
+        yLED = 1;
+        gLED = 0; 
+
+
+      }
   }
-  // if start = 0 anywhere
-  //This is essentially just disabled
-  else{
-    fan = 0; 
-    //disabled = 1;
-    moniWater = 0;
-    moniTemp = 0;
-
-    rLED = 0;
-    bLED = 0;
-    yLED = 1;
-    gLED = 0; 
 }
 
 
 
-//Work on next
+bool checkWater(){}
 
-double checkWater(){
-  //Use water level sensor from kit
-
-}
-
-double checkTemp(){
-// Use temperature and humidity module.
-}
+bool checkTemp(){}
